f409157f3136582d734ae0693c1addffea424f7e
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-06-11 00:02:20 -0500

    Add correct images and isbns

--- www/js/services.js
+++ www/js/services.js
@@ -3,9 +3,9 @@ angular.module('starter.services', [])
 .factory('Books', function() {
 
   var books = [
-    { title: "Nez Rouges Blouses Blanches", isbn: "9782753023345", id: 1 },
-    { title: "Le Dictionnaire Visuel", isbn: "9782753023345", id: 2 },
-    { title: "100 infos – Vitesse", isbn: "9782753023345", id: 3 },
+    { title: "Nez Rouges Blouses Blanches", isbn: "9782874491245", image_name: "nez_rouges.jpg", id: 1 },
+    { title: "Le Dictionnaire Visuel", isbn: "9782764410776", image_name: "Visuel4_Fr_An.jpg", id: 2 },
+    { title: "100 infos – Vitesse", isbn: "9782753023345", image_name: "piccolia-3345.jpg", id: 3 },
   ];
 
   return {
