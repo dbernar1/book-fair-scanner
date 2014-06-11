b6ae37954b73e70c6b60d15774b279b10b29be24
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-06-10 22:44:28 -0500

    Have basic list route working...

---
+++ www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope) {
+
+})
+
+.controller('CartCtrl', function($scope) {
+
+})
+
+.controller('CartCtrl', function($scope, $stateParams) {
+  
+})
